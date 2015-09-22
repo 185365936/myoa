4eaec55480c61f908547f7bf7e54a84edf9cf13c
woods <yellowwoods@live.com>
2015-09-22 15:20:05 +0800

    测试Servlet

---
+++ src/main/java/com/woods/myoa/servlet/ServletTest.java
@@ -0,0 +1,25 @@
+package com.woods.myoa.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by C on 2015/9/22.
+ *
+ * ≤‚ ‘Servlet
+ */
+public class ServletTest extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
