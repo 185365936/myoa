7e9011637619a1116f9481f1ef49240205fc7db2
woods <yellowwoods@live.com>
2015-09-22 15:02:02 +0800

    提交用户模块。

---
+++ src/main/java/com/woods/myoa/model/User.java
@@ -0,0 +1,19 @@
+package com.woods.myoa.model;
+
+/**
+ * Created by HuangLei on 2015/9/22.
+ *
+ * ”√ªß
+ */
+public class User {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
