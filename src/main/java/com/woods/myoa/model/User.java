801232c0487aec3381158b402a3de0349dcedc4d
woods <yellowwoods@live.com>
2015-09-23 20:30:58 +0800

    测试其他开发用户clone and push

--- src/main/java/com/woods/myoa/model/User.java
+++ src/main/java/com/woods/myoa/model/User.java
@@ -10,6 +10,9 @@ public class User {
     /** 用户名 */
     private String name;
 
+    /** 年龄 **/
+    private Integer age;
+
     /** 获取用户名 */
     public String getName() {
         return name;
@@ -20,4 +23,13 @@ public class User {
         this.name = name;
     }
 
+    /** 年龄 **/
+    public Integer getAge() {
+        return age;
+    }
+
+    /** 年龄 **/
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
